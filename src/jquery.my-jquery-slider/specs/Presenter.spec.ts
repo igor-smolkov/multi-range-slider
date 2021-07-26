702546047deaf421855fa2fafccc23cab23621f5
Igor Smolkov <smolkov.igor@gmail.com>
2021-07-26 16:15:18 +0700

    Протестировал презентер

--- src/jquery.my-jquery-slider/specs/Presenter.spec.ts
+++ src/jquery.my-jquery-slider/specs/Presenter.spec.ts
@@ -2,11 +2,86 @@
  * @jest-environment jsdom
  */
 
-import { Presenter } from "../Presenter";
+import $ from 'jquery';
+import { IPresenter, Presenter } from "../Presenter";
+import { TMyJQuerySlider } from "../TMyJQuerySlider";
 
-const rootElem = document.createElement('div');
-
-it('Инстанс должен быть создан', () => {
-  const presenter = new Presenter(rootElem);
-  expect(presenter).toBeDefined();
+describe('Презентер', () => {
+  // - подготовка
+  let 
+    rootElem: HTMLElement, 
+    $rootElem: JQuery<HTMLElement>,
+    fullConfig: TMyJQuerySlider;
+  beforeAll(() => {
+    fullConfig = {
+      min: 10,
+      max: 90,
+      value: 40,
+      step: 2,    
+      orientation: 'vertical',
+      isDouble: true,
+      minInterval: 40,
+      maxInterval: 60,
+      limits: [10, 40, 60, 90],
+      active: 0,
+      withLabel: true,
+      label: 'name',
+      scale: 'numeric',
+      list: [[10, 'яблоко'],[90, 'арбуз']],
+      actuals: [1],
+      lengthPx: 1000,
+      withIndent: false,
+    }
+  })
+  beforeEach(() => {
+    rootElem = document.createElement('div');
+    $rootElem = $(rootElem);
+  })
+  it('Инстанс должен быть создан', () => {
+    let presenter: IPresenter;
+    // - действие
+    presenter = new Presenter(rootElem);
+    // - проверка
+    expect(presenter).toBeDefined();
+  })
+  it('На элементе jQuery должно отработать событие инициализации', () => {
+    let presenter: IPresenter;
+    const initCallback: jest.Mock = jest.fn();
+    $rootElem.on('my-jquery-slider-init', initCallback);
+    // - действие
+    presenter = new Presenter(rootElem);
+    // - проверка
+    expect(initCallback).toBeCalledTimes(1);
+  })
+  it('На элементе jQuery должно отработать событие обновления', () => {
+    const presenter: IPresenter = new Presenter(rootElem);
+    const updateCallback: jest.Mock = jest.fn();
+    $rootElem.on('my-jquery-slider-update', updateCallback);
+    // - действие
+    presenter.update();
+    // - проверка
+    expect(updateCallback).toBeCalledTimes(1);
+  })
+  it('Поле данных элемента jQuery должено содержать все поля типа слайдера после его инициализации', () => {
+    let presenter: IPresenter;
+    const keysOfSlider: string[] = Object.keys(fullConfig);
+    // - действие
+    presenter = new Presenter(rootElem);
+    // - проверка
+    const keysOfRootData = Object.keys($rootElem.data());
+    keysOfSlider.forEach(key => {
+      expect(keysOfRootData).toContain(key);
+    })
+  })
+  it('Поле данных элемента jQuery должено содержать все поля типа слайдера после его обновления', () => {
+    const presenter: IPresenter = new Presenter(rootElem);
+    const keysOfSlider: string[] = Object.keys(fullConfig);
+    // - действие
+    presenter.update();
+    // - проверка
+    const keysOfRootData = Object.keys($rootElem.data());
+    keysOfSlider.forEach(key => {
+      expect(keysOfRootData).toContain(key);
+    })
+  })
 })
