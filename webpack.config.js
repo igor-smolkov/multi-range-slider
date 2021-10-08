ee4a719ef6516439560ed6914c17c64204e316dc
Igor Smolkov <smolkov.igor@gmail.com>
2021-10-08 16:17:41 +0700

    Added build, lint, test commands to Readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// eslint-disable-next-line import/no-unresolved
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // определение режима сборки
